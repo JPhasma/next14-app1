692274158b461e125847aab4a0f69bd3dbaa8eaa
Jane Dalton <jane@grubby.co.uk>
2024-01-02 16:30:54 +0000

    added usePathname hook for setting active class in nev

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: 'App1 | Blog',
+    description: 'From NextJS 14 Udemy course by Mohamed Hajji',
+};
+
 export default function BlogPage() {
     return (
         <div><h1>Blog</h1></div>
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+    const pathname = usePathname();
     return (
         <header className="header">
             <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/dashboard">Dashboard</Link></li>
-                <li><Link href="blog">Blog</Link></li>
+                <li className={`link ${pathname === '/' ? 'active' : ''}`} >
+                    <Link href="/">Home</Link>
+                </li>
+                <li className={`link ${pathname === '/dashboard' ? 'active' : ''}`} >
+                    <Link href="/dashboard">Dashboard</Link>
+                </li>
+                <li className={`link ${pathname === '/blog' ? 'active' : ''}`} >
+                    <Link href="blog">Blog</Link>
+                </li>
             </ul>
         </header>
     );
